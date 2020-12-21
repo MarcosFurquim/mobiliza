c91f294226c855339d72cff4bb0d632fb9a9ed74
Marcos Furquim <marcosfurquim@gmail.com>
2020-12-21 04:23:32 -0300

    ajustes

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -2,7 +2,17 @@ window.jQuery = window.$ = require("jquery");
 require("bootstrap");
 import Splide from "@splidejs/splide";
 
-new Splide(".splide", {
+var splide = new Splide(".splide", {
   type: "loop",
   perPage: 2,
+  perMove: 1,
+  gap: "30px",
+  pagination: false,
+  //   padding: {
+  //     right: "5rem",
+  //     left: "5rem",
+  //   },
 }).mount();
+splide.on("mounted", function () {
+  // This won't be executed.
+});
